9aed83c4ebf7f72ec847bc116641027a6c41e5c6
TusharChaure <chauretushar444@gmail.com>
2020-06-08 10:31:36 +0530

    new-changes-heroku

--- front-end/src/Cart.js
+++ front-end/src/Cart.js
@@ -1,92 +1,92 @@
-import React, {Component} from 'react'
-import axios from 'axios'
-import Cartlist from './Cartlist'
-import Button from 'react-bootstrap/Button'
+// //import React, {Component} from 'react'
+// //import axios from 'axios'
+// //import Cartlist from './Cartlist'
+// import Button from 'react-bootstrap/Button'
  
-class Cart extends Component { 
+// class Cart extends Component { 
   
-    handle() {
-        console.log("clicked");
-        axios.post('http://localhost:5000/order/add',
-        {
-            "orderid": "forder55",
-            "catogary": "Indian",
-                    "product": [
-                        {
+//     handle() {
+//         console.log("clicked");
+//         axios.post('http://localhost:5000/order/add',
+//         {
+//             "orderid": "forder55",
+//             "catogary": "Indian",
+//                     "product": [
+//                         {
                            
-                            "itemid": "item5",
-                            "quantity": 2,
-                            "totalprice": 60,
-                            "detailsifany": "no"
-                        },
-                        {
+//                             "itemid": "item5",
+//                             "quantity": 2,
+//                             "totalprice": 60,
+//                             "detailsifany": "no"
+//                         },
+//                         {
                            
-                            "itemid": "item7",
-                            "quantity": 2,
-                            "totalprice": 60,
-                            "detailsifany": "no"
-                        }
-                        ],
-                        "paymentType": "cash",
-                    "totalCost": 120
-        },
-        {
-            headers: {
-              'auth-token': localStorage.usertoken
-            }},
-            )
-            .then(res => {
-                alert('order!')
-            })
-            .catch(err => {
-                console.log("err")
-            })
-      }
+//                             "itemid": "item7",
+//                             "quantity": 2,
+//                             "totalprice": 60,
+//                             "detailsifany": "no"
+//                         }
+//                         ],
+//                         "paymentType": "cash",
+//                     "totalCost": 120
+//         },
+//         {
+//             headers: {
+//               'auth-token': localStorage.usertoken
+//             }},
+//             )
+//             .then(res => {
+//                 alert('order!')
+//             })
+//             .catch(err => {
+//                 console.log("err")
+//             })
+//       }
 
 
-    render() {
+//     render() {
 
-       const items  = [ 
+//        const items  = [ 
         
-            {
-                id:'',
-                item:"",
-                categary:"",
-                subcategary:"",
-                price:""
-            },
+//             {
+//                 id:'',
+//                 item:"",
+//                 categary:"",
+//                 subcategary:"",
+//                 price:""
+//             },
             
-        ]
+//         ]
 
-        const arr = items.map( (arraycard, i) => {
+//         const arr = items.map( (arraycard, i) => {
 	
-            return <Cartlist 
-                     id={items[i].id}
-                     item={items[i].item}
-                     categary={items[i].categary}
-                     subcategary={items[i].subcategary}
-                     price={items[i].price} 	
-                />
+//             return <Cartlist 
+//                      id={items[i].id}
+//                      item={items[i].item}
+//                      categary={items[i].categary}
+//                      subcategary={items[i].subcategary}
+//                      price={items[i].price} 	
+//                 />
         
-        } )
+//         } )
 
-    return (
+//     return (
         
-        <div style={{border:'solid', borderRadius:'10px',marginLeft:'10%',marginRight:'10%'}}>
-        <h2>Cart</h2>
-        <div style={{marginLeft:'0.5%', marginTop:'1%',marginRight:'0.5%',marginBottom:'15%'}}>
-        <h3 style={{textAlign:'left',marginLeft:'0.5%'}}> Selected Items : </h3>
-        <div style={{border:'solid', borderRadius:'10px',marginLeft:'0.5%',marginRight:'0.5%',boxShadow:'7px 7px rgb(167,167,167)'}}>
-            {arr} 
-        </div>
-        <div style={{marginLeft:'5%',marginRight:'5%',marginTop:'3%'}}>
-            <button type="button" onClick={this.handle} variant="secondary" style={{float:'right', borderRadius:'10px'}}>Place Order</button>
-        </div>
-        </div>
-        </div>
-    )
-    }
-}
+//         <div style={{border:'solid', borderRadius:'10px',marginLeft:'10%',marginRight:'10%'}}>
+//         <h2>Cart</h2>
+//         <div style={{marginLeft:'0.5%', marginTop:'1%',marginRight:'0.5%',marginBottom:'15%'}}>
+//         <h3 style={{textAlign:'left',marginLeft:'0.5%'}}> Selected Items : </h3>
+//         <div style={{border:'solid', borderRadius:'10px',marginLeft:'0.5%',marginRight:'0.5%',boxShadow:'7px 7px rgb(167,167,167)'}}>
+//             {arr} 
+//         </div>
+//         <div style={{marginLeft:'5%',marginRight:'5%',marginTop:'3%'}}>
+//             <button type="button" onClick={this.handle} variant="secondary" style={{float:'right', borderRadius:'10px'}}>Place Order</button>
+//         </div>
+//         </div>
+//         </div>
+//     )
+//     }
+// }
   
 
-export default Cart;
+// export default Cart;
--- front-end/src/Cartlist.js
+++ front-end/src/Cartlist.js
@@ -1,47 +1,46 @@
-import React from 'react';
-import 'tachyons';
-import Table from 'react-bootstrap/Table'
-import { FaEdit } from 'react-icons/fa'
+// import React from 'react';
+// import 'tachyons';
+// import Table from 'react-bootstrap/Table'
+// import { FaEdit } from 'react-icons/fa'
 
 
-function handleClick() {
-    console.log('Button click ...');
-    localStorage.removeItem("items");
-    localStorage.removeItem("cat");
-    localStorage.removeItem("price");
-    window.location.reload(1000);
-  }
+// function handleClick() {
+//     console.log('Button click ...');
+//     localStorage.removeItem("items");
+//     localStorage.removeItem("cat");
+//     localStorage.removeItem("price");
+//     window.location.reload(1000);
+//   }
 
-const Cartlist = (props) => {
+// const Cartlist = (props) => {
 
     
-    return (  
-        
-        <div >
+//  
+//         <div >
           
-          <Table striped bordered hover >
-                <thead>
-                    <tr>
-                    <th>Item</th>
-                    <th>Cat</th>
-                    <th>Price</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <td>{localStorage.items}</td>
-                    <td>{localStorage.cat}</td>
-                    <td>{localStorage.price}</td>
-                    <td><button type="button" onClick={handleClick}  variant="primary" ><FaEdit/></button></td>
-                    </tr>
-                </tbody>
-            </Table>
+//           <Table striped bordered hover >
+//                 <thead>
+//                     <tr>
+//                     <th>Item</th>
+//                     <th>Cat</th>
+//                     <th>Price</th>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     <tr>
+//                     <td>{localStorage.items}</td>
+//                     <td>{localStorage.cat}</td>
+//                     <td>{localStorage.price}</td>
+//                     <td><button type="button" onClick={handleClick}  variant="primary" ><FaEdit/></button></td>
+//                     </tr>
+//                 </tbody>
+//             </Table>
             
-        </div>
+//         </div>
         
                                      
             
-    )
-}
+//     )
+// }
 
-export default Cartlist;
+// export default Cartlist;
